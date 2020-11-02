1b7d12377716354288f8ab3b14967c8b66e2cd84
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2020-11-02 11:41:25 +0530

    add footer.php file

---
+++ codeigniter/application/views/footer.php
@@ -0,0 +1,22 @@
+<footer class="page-footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-md-8 col-sm-12">
+				<h6 class="text-uppercase font-weight-bold">Additional Information on RasaRecipe</h6>
+					<p>Of course, there is more to learn. visit our Rasarecipe page on facebook. </p>
+					<p>We’re here for our customers, and want each one of them to have a unique, one-of-a-kind experience at our amazing Cake Shop.</p>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-12">
+				<h6 class="text-uppercase font-weight-bold">Contact</h6>
+					<p>35, Colombo 5
+					<br/>info@rasarecipe.com
+					<br/>+ 01 234 567 88
+					<br/>+ 01 234 567 89</p>
+			</div>
+		</div>
+		<div class="footer-copyright text-center">© 2019 Copyright:
+		RasaRecipeWebsite.com</div>
+</footer>
+</body>
+
+</html>
