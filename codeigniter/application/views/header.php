ea98f3082007ee16e83b11ca1d792cd64f6bfcd8
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2020-11-02 11:42:19 +0530

    add header.php file

---
+++ codeigniter/application/views/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+	<script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap.js');?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/js/jQuery -3.5.1.js');?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/js/main.js');?>"></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.css');?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/main.css');?>">
+</head>
+<body>
+
+<nav class="navbar navbar-light bg-primary navbar-expand-md">
+				<a class="navbar-brand" href="#"><img src="assets/images/Yummy recipe (3)1.png" class="logo"></a>
+				<button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse"
+		data-target="#main-navigation">
+				 	<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="main-navigation">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a class="nav-link" href="#">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">About</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Contact</a>
+						</li>
+					</ul>
+				</div>
+		</nav>
+
+		
